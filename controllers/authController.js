3a41ceddccab7f031bfb9cec29baf23d2cffe420
bukarinevgeni <veliknon@gmail.com>
2024-04-06 21:57:40 +0300

    refresh

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,34 @@ module.exports.login = async(req, res) => {
           return res.status(400).send({ error: 'Invalid login credentials' });
         }
         const token = authMiddleware.generateAccessToken(username);
-        res.status(201).json(token);
+        const refreshToken = authMiddleware.generateRefreshToken(username);
+        res.status(201).json(
+          {
+            accessToken: token,
+            refreshToken: refreshToken
+          }
+        );
+      } catch (error) {
+        res.send(error);
+      }
+}
+
+mmodule.exports.refreshToken = async(req, res) => {
+    try {
+        const { refreshToken } = req.body;
+        if (!refreshToken) {
+          return res.status(401).send({ error: 'Refresh token is required' });
+        }
+        if (!refreshTokens.includes(refreshToken)) {
+          return res.status(403).send({ error: 'Refresh token is not valid' });
+        }
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+          if (err) {
+            return res.status(403).send({ error: 'Refresh token is not valid' });
+          }
+          const accessToken = generateAccessToken({ username: user.username });
+          res.json({ accessToken });
+        });
       } catch (error) {
         res.send(error);
       }
